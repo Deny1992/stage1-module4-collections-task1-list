b54ab969745e11bb3a9360c6cd2d1ff43a2d5ea0
Denyonepeny <denisshulga1992@gmail.com>
2023-01-17 18:38:41 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,25 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aValue = getValue(a);
+        int bValue = getValue(b);
+
+        if(aValue == bValue){
+            return Integer.parseInt(a) - Integer.parseInt(b);
+        }
+
+        return getValue(a) - getValue(b);
+    }
+
+    private int getValue(String a){
+        int i = Integer.parseInt(a);
+        return 5 * i * i + 3;
     }
 }
